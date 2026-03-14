a50f176fada55ccf45a47a8554f74f0c85a8c0e1
Shenandoah-Walker <saw67402@email.vccs.edu>
2026-03-14 19:41:58 +0000

    updated ParkingMeter class (added try-catch blocks, fixed part with default minutes = 60

--- ParkingMeter.py
+++ ParkingMeter.py
@@ -2,22 +2,21 @@
 #• Number of minutes of parking time purchased
 
 class ParkingMeter:
-   #This method takes inputs to create a ParkingMeter object that contains the number of minutes of parking time purchased.
-   def __init__(self, minutes_purchased):
-      self.__minutes_purchased = 60
-      return self.__minutes_purchased
+    #This method takes inputs to create a ParkingMeter object that contains the number of minutes of parking time purchased.
+    def __init__(self, minutes_purchased=60):
+        self.set_minutes_purchased(minutes_purchased)
+
     #This method returns the number of minutes of parking time purchased.
-   def get_minutes_purchased(self):
-      return self.__minutes_purchased
+    def get_minutes_purchased(self):
+        return self.__minutes_purchased
 
-   #This method sets the number of minutes of parking time purchased. It also validates the input to ensure it is a positive number.
-   def set_minutes_purchased(self, minutes_purchased, minutes):
-    if (minutes > 0):
-      self.__minutes_purchased = minutes
-    else:
-        print("Invalid minutes")
-        self.__minutes_purchased = 0
-
-    return self.__minutes_purchased
+    #This method sets the number of minutes of parking time purchased. It also validates the input to ensure it is a positive number.
+    def set_minutes_purchased(self, minutes):
+        try:
+            if minutes <= 0:
+                raise ValueError("Minutes purchased must be greater than 0.")
+            self.__minutes_purchased = minutes
+        except ValueError as e:
+            print(e)
 
 
