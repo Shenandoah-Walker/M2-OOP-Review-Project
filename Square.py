d6173aaf40e95406ff639ac37a26e1c4efd1d491
Shenandoah-Walker <saw67402@email.vccs.edu>
2026-03-14 22:05:06 +0000

    Wrote square class

---
+++ Square.py
@@ -0,0 +1,27 @@
+#Square class
+
+from Rectangle import Rectangle
+
+class Sqaure(Rectangle):
+
+   #This method will contain 1 protected attribute: side
+   def __init__(self, side, name = "Square"):
+      self._side = side
+      super().__init__(side, side, name)
+      self.name = name
+
+
+  #This method will return the side of the square
+   @property
+   def side(self):
+       return self._side
+
+  #This method will set the side of the square
+   @side.setter
+   def side(self, side):
+      self._side = side
+      self.length = side
+      self.width = side
+
+
+
