1ac0dec47f53bc99561ed328793470288dadf558
Shenandoah-Walker <saw67402@email.vccs.edu>
2026-03-14 19:12:08 +0000

    finished PoliceOfficer class

--- PoliceOfficer.py
+++ PoliceOfficer.py
@@ -1,11 +1,23 @@
 class  PoliceOfficer:
 
+   #inherit the the ParkedCar class and the ParkingMeter class
+   from ParkedCar import ParkedCar
+   from ParkingMeter import ParkingMeter
+
   #This method takes inputs to create a PoliceOfficer object that contains the officer's name and badge number.
 
   def __init__(self, name, badge_number):
      self.__name = name
      self.__badge_number = badge_number
 
-  def inspect_car(self, car, meter)
-   #This method takes a ParkedCar object and a ParkingMeter object as arguments. It checks if the car's time has expired and if so, issues a parking ticket. It inherits the car from the Parked ar class and the meter from the ParlkingMeter class.
-  
+   #This method takes a ParkedCar object and a ParkingMeter object as arguments. It checks if the car's time has expired and if so, tells the ParkingTicket class to issue a ticket.
+
+  def inspect_car(self, car, meter, illegal):
+      if car.get_minutes_parked() > meter.get_minutes_purchased():
+          illegal = True
+          return illegal     
+         
+      else:
+          print("No ticket issued.")
+          return None
+
