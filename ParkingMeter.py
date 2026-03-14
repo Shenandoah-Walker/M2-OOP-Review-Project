0b8041d11a7de79754d4e3a44a25b1a4dcda3874
Shenandoah-Walker <saw67402@email.vccs.edu>
2026-03-14 13:09:53 +0000

    Addad parking meter class

---
+++ ParkingMeter.py
@@ -0,0 +1,9 @@
+#Responsible for knowing:
+#• Number of minutes of parking time purchased
+
+class ParkingMeter:
+   #This method takes inputs to create a ParkingMeter object that contains the number of minutes of parking time purchased.
+   def __init__(self, minutes_purchased):
+      self.__minutes_purchased = minutes_purchased
+      self.__minutes_purchased = minutes_purchased
+      return self.__minutes_purchased
