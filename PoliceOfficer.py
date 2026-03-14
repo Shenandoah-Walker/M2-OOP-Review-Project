d150f3055a75895b42198f6a2cdd3f838a081051
Shenandoah-Walker <saw67402@email.vccs.edu>
2026-03-14 17:05:52 +0000

    Created and started working on PoliceOfficer class

---
+++ PoliceOfficer.py
@@ -0,0 +1,11 @@
+class  PoliceOfficer:
+
+  #This method takes inputs to create a PoliceOfficer object that contains the officer's name and badge number.
+
+  def __init__(self, name, badge_number):
+     self.__name = name
+     self.__badge_number = badge_number
+
+  def inspect_car(self, car, meter)
+   #This method takes a ParkedCar object and a ParkingMeter object as arguments. It checks if the car's time has expired and if so, issues a parking ticket. It inherits the car from the Parked ar class and the meter from the ParlkingMeter class.
+  
