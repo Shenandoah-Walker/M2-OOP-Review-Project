86e1982eaf3fc6895c6141ac87cae0c7466f90a7
Shenandoah-Walker <saw67402@email.vccs.edu>
2026-03-14 21:30:19 +0000

    Finished BasicShape class

--- BasicShape.py
+++ BasicShape.py
@@ -10,12 +10,24 @@ class BasicShape(ABC):
      self._name = name
 
   #This method will return the area of the shape
-   def getArea(self):
+   @property
+   def area(self):
       return self._area
+
+  #This method will set the area of the shape
+   @area.setter
+   def area(self, area):
+      self._area = area
      
   #This method will return the name of the shape
-   def getName(self):
+   @property
+   def name(self):
       return self._name
+
+  #This method will set the name of the shape
+   @name.setter
+   def name(self, name):
+      self._name = name
 
   #This method will calculate the area of the shape. It will be overridden by the subclasses
    @abstractmethod
